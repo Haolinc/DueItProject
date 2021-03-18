f8901b4d399cd67fa34a3c311e3771480e7e9995
Haolin <forskyrimdemacia@gmail.com>
2021-03-17 21:40:53 -0400

    Added a test button to create checkbox in duedate column in scrollview

--- app/src/main/java/com/example/duelt/MainActivity.java
+++ app/src/main/java/com/example/duelt/MainActivity.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,5 +28,19 @@ public class MainActivity extends AppCompatActivity {
     public void jumpToMini(View v){
         Intent i = new Intent(this, MiniActivity.class);
         startActivity(i);
+    }
+
+
+    //use ScrollView as parent and call linearlayout for action, change ScrollView values in xml files
+    //Orientation is set in xml file
+    public void createCheckBox(View v) {
+        CheckBox cb = new CheckBox(this);
+        LinearLayout dueDate = findViewById(R.id.dueDate_layout);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(-2, -2);  //wrap_content
+
+        cb.setText("new");
+        cb.setLayoutParams(lp);
+        cb.setGravity(Gravity.CENTER_VERTICAL);
+        dueDate.addView(cb);
     }
 }
