47678f1db3fce16ca699bbf8bfea6b3e7c4dd968
Haolin <forskyrimdemacia@gmail.com>
2021-03-17 23:31:40 -0400

    Added alert mechanic and delete checkbox mechanic when checked the checkbox

--- app/src/main/java/com/example/duelt/MainActivity.java
+++ app/src/main/java/com/example/duelt/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.duelt;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         startActivity(i);
     }
 
+
 //testing
     
     public void jumpToMini(View v){
@@ -33,9 +39,54 @@ public class MainActivity extends AppCompatActivity {
 
     //use ScrollView as parent and call linearlayout for action, change ScrollView values in xml files
     //Orientation is set in xml file
-    public void createCheckBox(View v) {
+    public void createCheckBoxInDueDate(View v) {
         CheckBox cb = new CheckBox(this);
         LinearLayout dueDate = findViewById(R.id.dueDate_layout);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(-2, -2);  //wrap_content
+
+        cb.setText("new");
+        cb.setLayoutParams(lp);
+        cb.setGravity(Gravity.CENTER_VERTICAL);
+        cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){      //when checked
+            @Override
+            public void onCheckedChanged(CompoundButton button, boolean isChecked) {
+                if (isChecked) {
+                    AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                    alertDialog.setTitle("alert");
+                    alertDialog.setMessage("Are you sure you want to delete this?");
+                    alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Yes",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dueDate.removeView(cb);   //click to remove checkbox view
+                                }
+                            });
+                    alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "No",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    cb.setChecked(false);
+                                }
+                            });
+                    alertDialog.show();
+                }
+            }
+        });
+        dueDate.addView(cb);
+
+    }
+
+    public void deleteCheckBoxInDueDate(View v){
+
+    }
+
+    public void deleteOnCheck(View v){
+
+    }
+
+    public void createCheckBoxInReminder(View v) {
+        CheckBox cb = new CheckBox(this);
+        LinearLayout dueDate = findViewById(R.id.reminder_layout);
         LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(-2, -2);  //wrap_content
 
         cb.setText("new");
