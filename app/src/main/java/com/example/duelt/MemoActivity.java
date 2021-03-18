a65ad262ffe8a7b0e57f0e846e52d2cb4fc7a312
DESKTOP-UAHTL3T\zejun <francisren1998@gmail.com>
2021-03-17 22:07:21 -0400

    edit memoAct  (test code)

--- app/src/main/java/com/example/duelt/MemoActivity.java
+++ app/src/main/java/com/example/duelt/MemoActivity.java
@@ -1,13 +1,24 @@
 package com.example.duelt;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MemoActivity extends AppCompatActivity {
 
+
+/*test code*/
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MemoActivity extends AppCompatActivity {
+    //test code
+    private Date date;
+    private String text;
+    private static DateFormat dateFormate = new SimpleDateFormat("dd/MM/yyy 'at' hh:mm aaa");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,4 +34,37 @@ public class MemoActivity extends AppCompatActivity {
 
         finish();
     }
+
+
+    //testcode
+
+    public MemoActivity(){
+        this.date = new Date();
+    }
+
+    public MemoActivity(int time, String text) {
+        this.date = new Date(time);
+        this.text = text;
+    }
+
+    public String getDate(){
+        return dateFormate.format(date);
+    }
+
+    public long getTime() {
+        return date.getTime();
+    }
+
+    public void setTime(int time) {
+        this.date = new Date(time);
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getText(){
+        return this.text;
+    }
+
 }
