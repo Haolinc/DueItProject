79b71832cdaf3e7f6cd39efdeb90e9f1011e5821
DESKTOP-UAHTL3T\zejun <francisren1998@gmail.com>
2021-03-28 23:24:22 -0400

    added gif image and still working on it

--- app/src/main/java/com/example/duelt/TreatmentActivity.java
+++ app/src/main/java/com/example/duelt/TreatmentActivity.java
@@ -1,21 +1,45 @@
 package com.example.duelt;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.widget.Button;
+
+import pl.droidsonroids.gif.GifImageView;
 
 public class TreatmentActivity extends AppCompatActivity {
+    GifImageView gifImageView;
+    Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_treatment);
+
+        //gitsetting
+       /* gifImageView = (GifImageView)findViewById(R.id.countDown3);
+        btnStart = (Button) findViewById(R.id.btnStart);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gifImageView.getDrawable();
+            }
+        }); */
     }
 
     public void back(View v){
         finish();
     }
 
-
+    public void onClick(View v){
+        v.setVisibility(View.GONE);
+        gifImageView = (GifImageView)findViewById(R.id.countDown3);
+        gifImageView.setVisibility(View.VISIBLE);
+    }
+    
 }
+
