af10888294f9ef8f903165e4f1a0fabb9febb9f1
Haolin <forskyrimdemacia@gmail.com>
2021-04-02 15:15:51 -0400

    Alarm fixed

--- app/src/main/java/com/example/duelt/AlarmReceiver.java
+++ app/src/main/java/com/example/duelt/AlarmReceiver.java
@@ -1,8 +1,12 @@
 package com.example.duelt;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.SystemClock;
+import android.view.View;
 import android.widget.Toast;
 
 public class AlarmReceiver extends BroadcastReceiver {
--- app/src/main/java/com/example/duelt/DailyActivity.java
+++ app/src/main/java/com/example/duelt/DailyActivity.java
@@ -7,29 +7,39 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.icu.number.Scale;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.view.View;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.util.Calendar;
+
 public class DailyActivity extends AppCompatActivity {
     private TimePicker tp;
-    public void back(View v){
-        finish();
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily);
         tp = findViewById(R.id.datePicker1);
     }
+    public void back(View v){
+        finish();
+    }
 
     public void setAlarm(View view) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(tp.)
         AlarmManager am = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
         Intent i = new Intent(this, AlarmReceiver.class);
-        PendingIntent pi = PendingIntent.getBroadcast(this, 0, i, 0);
-        am.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime()+1000, pi);
+        PendingIntent pi = PendingIntent.getBroadcast(this, 1, i, 0);
+        am.setExact(AlarmManager.RTC_WAKEUP, 1000, pi);
+    }
+    public void cancelAlarm(View view) {
+        AlarmManager am = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+        Intent i = new Intent(this, AlarmReceiver.class);
+        PendingIntent pi = PendingIntent.getBroadcast(this, 1, i, 0);
+        am.cancel(pi);
     }
 }
