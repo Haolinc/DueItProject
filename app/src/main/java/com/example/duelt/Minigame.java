5096469e3e84fbb5161142a9914959aabdcca407
Haolin <forskyrimdemacia@gmail.com>
2021-04-05 20:16:50 -0400

    Combine petdatabase to database helper, fix bug on set, get, and delete table, still having issue on update

--- app/src/main/java/com/example/duelt/Minigame.java
+++ app/src/main/java/com/example/duelt/Minigame.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
+import com.example.duelt.db.DatabaseHelper;
 
 public class Minigame extends AppCompatActivity {
     private TextView petHungry;
@@ -64,16 +64,31 @@ public class Minigame extends AppCompatActivity {
     }
 
     private void updateText1(){
-        petHungry.setText("you will have a five minute break");
+        DatabaseHelper petDatabaseHelper = new DatabaseHelper(this);
+        PetModel petmodel = new PetModel(50, 50, 0, 1, "Boo");
+        petDatabaseHelper.addOne(petmodel);
+        String petmodelText = "New Pet Set";
+        petHungry.setText(petmodelText);
     }
     private void updateText2(){
-        petMood.setText("you will have a five minute break");
+        DatabaseHelper petDatabaseHelper = new DatabaseHelper(this);
+        PetModel petmodel = petDatabaseHelper.getCurrentStat();
+        String petmodelText = "From Database: " + " Name: "+ petmodel.getName() + " Hungriness: " + petmodel.getHungriness()
+                + " Exp: " + petmodel.getExp()+ " Level: " + petmodel.getLv() + " Mood: " + petmodel.getMood();
+        petMood.setText(petmodelText);
     }
     private void updateText3(){
-        petexp.setText("you will have a five minute break");
+        DatabaseHelper petDatabaseHelper = new DatabaseHelper(this);
+        PetModel oldPetmodel = petDatabaseHelper.getCurrentStat();
+        PetModel newPetModel = new PetModel(5, oldPetmodel.getMood(), oldPetmodel.getExp(), oldPetmodel.getExp(), oldPetmodel.getName());
+        petDatabaseHelper.updateData(newPetModel);
+        String petmodelText = "Made hungriness to 5 from database";
+        petexp.setText(petmodelText);
     }
     private void updateText4(){
-        petlv.setText("you will have a five minute break");
+        DatabaseHelper petDatabaseHelper = new DatabaseHelper(this);
+        petDatabaseHelper.killPet();
+        petlv.setText("Dropped table");
     }
 
 }
--- app/src/main/java/com/example/duelt/db/DatabaseHelper.java
+++ app/src/main/java/com/example/duelt/db/DatabaseHelper.java
@@ -9,6 +9,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 import com.example.duelt.EventDateModel;
+import com.example.duelt.PetModel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,7 +17,8 @@ import java.util.List;
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE = "duelt.db";
     public static final int VERSION  = 1;
-    private static final String TABLE_NAME = "EVENT_DATE_TABLE";
+    //Time table
+    private static final String TIME_TABLE_NAME = "EVENT_DATE_TABLE";
     private static final String TIME_FOR_ORDER_COLUMN = "TIME_FOR_ORDER";
     private static final String YEAR_COLUMN = "YEAR";
     private static final String MONTH_COLUMN = "MONTH";
@@ -26,6 +28,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String EVENT_TITLE_COLUMN = "EVENT_TITLE";
     private static final String EVENT_DETAIL_COLUMN = "EVENT_DETAIL";
     private static final String ID_COLUMN = "ID";
+    //Pet table
+    private static final String PET_TABLE_NAME = "PET_STAT_TABLE";
+    private static final String NAME_COLUMN = "NAME";
+    private static final String HUNGRINESS_COLUMN = "HUNGRINESS";
+    private static final String MOOD_COLUMN = "MOOD";
+    private static final String EXP_COLUMN = "EXP";
+    private static final String LEVEL_COLUMN = "LEVEL";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE, null, VERSION);
@@ -33,17 +42,85 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String createTableStatement = "CREATE TABLE " + TABLE_NAME + " ( " + TIME_FOR_ORDER_COLUMN
+        final String createTimeTableStatement = "CREATE TABLE IF NOT EXISTS " + TIME_TABLE_NAME + " ( " + TIME_FOR_ORDER_COLUMN
                 + " BIGINT, " + YEAR_COLUMN + " INT, " + MONTH_COLUMN + " INT, " + DAY_COLUMN +
                 " INT, " + HOUR_COLUMN + " INT, " + MINUTE_COLUMN + " INT, " + EVENT_TITLE_COLUMN +
                 " TEXT, " + EVENT_DETAIL_COLUMN + " TEXT, " + ID_COLUMN  + " INT UNIQUE);";
-        db.execSQL(createTableStatement);
+        db.execSQL(createTimeTableStatement);
+
+        final String createPetTableStatement = "CREATE TABLE IF NOT EXISTS " + PET_TABLE_NAME + " ( "
+                + NAME_COLUMN + " TEXT UNIQUE, "
+                + HUNGRINESS_COLUMN + " INT, "
+                + MOOD_COLUMN + " INT, "
+                + EXP_COLUMN + " INT, "
+                + LEVEL_COLUMN + " INT);";
+        db.execSQL(createPetTableStatement);
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + TIME_TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + PET_TABLE_NAME);
         onCreate(db);
+    }
+
+    public void updateData(PetModel pet){
+        SQLiteDatabase db = this.getReadableDatabase();
+        db.execSQL("UPDATE " + PET_TABLE_NAME + " SET "
+                + HUNGRINESS_COLUMN + " = "+ pet.getHungriness() + ", "
+                + MOOD_COLUMN + " = " + pet.getMood() + ", "
+                + EXP_COLUMN + " = " + pet.getExp() + ", "
+                + LEVEL_COLUMN + " = " + pet.getLv()
+                + " WHERE " + NAME_COLUMN + " = " + pet.getName() + ";");
+        db.close();
+    }
+
+    public void addOne(PetModel pet){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(NAME_COLUMN, pet.getName());
+        cv.put(HUNGRINESS_COLUMN, pet.getHungriness());
+        cv.put(MOOD_COLUMN, pet.getMood());
+        cv.put(EXP_COLUMN, pet.getExp());
+        cv.put(LEVEL_COLUMN, pet.getLv());
+
+        db.insert(PET_TABLE_NAME, null, cv);
+        db.close();
+    }
+
+    public void killPet(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("DROP TABLE IF EXISTS " + PET_TABLE_NAME + ";");
+        onCreate(db);
+        db.close();
+    }
+
+    public PetModel getCurrentStat(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        PetModel petStat = new PetModel(0, 0, 0,0, "");
+
+        Cursor cursor = db.rawQuery("SELECT * From  "+ PET_TABLE_NAME, null);
+
+        if(cursor.moveToFirst()){
+
+            do{
+                String name = cursor.getString(0);
+                int hunger = cursor.getInt(1);
+                int mood = cursor.getInt(2) ;
+                int exp = cursor.getInt(3);
+                int level = cursor.getInt(4);
+
+
+                petStat = new PetModel(hunger, mood, exp, level, name);
+
+            }while (cursor.moveToNext());
+        }
+
+        cursor.close();
+        db.close();
+        return petStat;
     }
 
     public void addOne(EventDateModel eventDateModel){
@@ -60,7 +137,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         cv.put(EVENT_DETAIL_COLUMN, eventDateModel.getEventDetail());
         cv.put(ID_COLUMN, eventDateModel.getID());
 
-        db.insert(TABLE_NAME, null, cv);
+        db.insert(TIME_TABLE_NAME, null, cv);
         db.close();
     }
 
@@ -69,7 +146,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
         String queryString = "DELETE FROM " +
                //TABLE_NAME+ " WHERE "  + YEAR_COLUMN + " = " + eventDateModel.getYear() + ";";
-                TABLE_NAME + " WHERE " + ID_COLUMN + " = " + eventDateModel.getID() + ";" ;
+                TIME_TABLE_NAME + " WHERE " + ID_COLUMN + " = " + eventDateModel.getID() + ";" ;
         Cursor cursor = db.rawQuery(queryString, null);
         if (cursor.moveToFirst()){
             cursor.close();
@@ -87,7 +164,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
         String queryString = "DELETE FROM " +
                 //TABLE_NAME+ " WHERE "  + YEAR_COLUMN + " = " + eventDateModel.getYear() + ";";
-                TABLE_NAME + " WHERE " + ID_COLUMN + " = " + id + ";" ;
+                TIME_TABLE_NAME + " WHERE " + ID_COLUMN + " = " + id + ";" ;
         Cursor cursor = db.rawQuery(queryString, null);
         if (cursor.moveToFirst()){
             cursor.close();
@@ -104,7 +181,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public void deleteAll() {
         SQLiteDatabase db = this.getWritableDatabase();
-        String queryString = "DELETE FROM "+TABLE_NAME + ";";
+        String queryString = "DELETE FROM "+TIME_TABLE_NAME + ";";
         Cursor cursor = db.rawQuery(queryString, null);
         cursor.close();
         db.close();
@@ -114,7 +191,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         List<EventDateModel> eventDateModelsList = new ArrayList<>();
         SQLiteDatabase db = this.getReadableDatabase();
 
-        Cursor cursor = db.rawQuery("SELECT * From  "+TABLE_NAME+" WHERE "+ TIME_FOR_ORDER_COLUMN + ">10000 " + " ORDER BY "+ TIME_FOR_ORDER_COLUMN, null);
+        Cursor cursor = db.rawQuery("SELECT * From  "+TIME_TABLE_NAME+" WHERE "+ TIME_FOR_ORDER_COLUMN + ">10000 " + " ORDER BY "+ TIME_FOR_ORDER_COLUMN, null);
 
         if(cursor.moveToFirst()){
 
@@ -143,7 +220,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         List<EventDateModel> eventDateModelsList = new ArrayList<>();
         SQLiteDatabase db = this.getReadableDatabase();
 
-        Cursor cursor = db.rawQuery("SELECT * From  "+TABLE_NAME+" WHERE "+ TIME_FOR_ORDER_COLUMN + "<10000 " + " ORDER BY "+ TIME_FOR_ORDER_COLUMN, null);
+        Cursor cursor = db.rawQuery("SELECT * From  "+TIME_TABLE_NAME+" WHERE "+ TIME_FOR_ORDER_COLUMN + "<10000 " + " ORDER BY "+ TIME_FOR_ORDER_COLUMN, null);
 
         if(cursor.moveToFirst()){
 
@@ -172,7 +249,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         List<EventDateModel> eventDateModelsList = new ArrayList<>();
         SQLiteDatabase db = this.getReadableDatabase();
 
-        Cursor cursor = db.rawQuery("SELECT * From  "+TABLE_NAME+" ORDER BY "+ TIME_FOR_ORDER_COLUMN, null);
+        Cursor cursor = db.rawQuery("SELECT * From  "+TIME_TABLE_NAME+" ORDER BY "+ TIME_FOR_ORDER_COLUMN, null);
 
         if(cursor.moveToFirst()){
 
@@ -201,7 +278,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         List<Integer> idList = new ArrayList<>();
         SQLiteDatabase db = this.getReadableDatabase();
 
-        Cursor cursor = db.rawQuery("SELECT " + ID_COLUMN + " From  "+TABLE_NAME, null);
+        Cursor cursor = db.rawQuery("SELECT " + ID_COLUMN + " From  "+TIME_TABLE_NAME, null);
 
         if(cursor.moveToFirst()){
 
