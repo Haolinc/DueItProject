67fb8a7842d6a9f708f49a6f7a1fd25b8fc22a02
DESKTOP-UAHTL3T\zejun <francisren1998@gmail.com>
2021-04-30 14:57:11 -0400

    finish up congradulation page

--- app/src/main/java/com/example/duelt/popWindows/Congratulation_Window.java
+++ app/src/main/java/com/example/duelt/popWindows/Congratulation_Window.java
@@ -4,16 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.duelt.R;
 import com.example.duelt.db.DatabaseHelper;
+import com.example.duelt.db.PetModel;
 import com.example.duelt.fragments.TreatmentFragment;
+
+import org.w3c.dom.Text;
 
 public class Congratulation_Window extends AppCompatActivity {
     AnimationDrawable mWinnerCup;
     private TextView mTextViewDuration;
+    private TextView mTextViewReward;
+    private TextView mTextViewRewardExp;
     boolean mWinnerCupIsRunning;
     long mTimer = TreatmentFragment.getmStartTimeInMillis();
     DatabaseHelper petDatabaseHelper = new DatabaseHelper(this);
@@ -26,19 +33,59 @@ public class Congratulation_Window extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_congratulation__window);
 
+        PetModel petmodel = petDatabaseHelper.getCurrentStat();
+
         ImageView vWinnerCup = (ImageView)findViewById(R.id.img_winner_cup);
         vWinnerCup.setBackgroundResource(R.drawable.winner_cup_list);
         mWinnerCup = (AnimationDrawable) vWinnerCup.getBackground();
         mWinnerCup.start();
         mTextViewDuration = findViewById(R.id.textview_duration);
+        mTextViewReward = findViewById(R.id.textview_reward2);
+        mTextViewRewardExp = findViewById(R.id.textview_reward);
 
         mTextViewDuration.setText(mTimer+" min");
 
+        mTextViewReward.setText("you gain " + currencyGain(mTimer) + " dollar");
+
+        mTextViewRewardExp.setText("you gain " + ExpGain(mTimer) + " exp");
+
+        addCurrency();
+        addExp();
+    }
+
+
+    private void addExp(){
+        PetModel petModel = petDatabaseHelper.getCurrentStat();
+        int gotExp = ExpGain(mTimer);
+        petModel.expPlus(this, gotExp);
+        petDatabaseHelper.updateData(petModel);
+    }
+
+    private int ExpGain(long mTimer){
+        int expGain = (int)mTimer/6;
+        return expGain;
+    }
+
+    private void addCurrency(){
+        int totalCurrency = petDatabaseHelper.getCurrency();
+        int moneyGain = currencyGain(mTimer);
+
+        totalCurrency = moneyGain + totalCurrency;
+
+        petDatabaseHelper.updateCurrency(totalCurrency);
+
+    }
+
+
+    private int currencyGain(long mTimer){
+        int moneyGain = (int)mTimer/ 6;
+        return moneyGain;
     }
 }
 
 
-//    private void getCurrentStat(){
+//    private void getCurrentStat(){{
+
 //        PetModel petmodel = petDatabaseHelper.getCurrentStat();
 //        String petmodelText = "From Database: " + " Name: "+ petmodel.getName() + " Hungriness: " + petmodel.getHungriness()
 //                + " Exp: " + petmodel.getExp()+ " Level: " + petmodel.getLv() + " Mood: " + petmodel.getMood();
