9b25e38253c7d56d45dcbf2c6a31ee2d0ae1242c
Junquan wu <wujunquan233@gmail.com>
2021-05-02 21:29:38 -0400

    add shop function and pic. toy and food

--- app/src/main/java/com/example/duelt/fragments/MiniFragment.java
+++ app/src/main/java/com/example/duelt/fragments/MiniFragment.java
@@ -1,6 +1,7 @@
 package com.example.duelt.fragments;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -25,7 +27,7 @@ public class MiniFragment extends Fragment {
     private StatesView expState;
     private TextView petLvNum;
     private Button states_pup_up_cancel_btn;
-
+    DatabaseHelper petDatabaseHelper;
 
     public MiniFragment(){
         //Required empty public constructor
@@ -37,6 +39,7 @@ public class MiniFragment extends Fragment {
 
         //Buttons in Fragments should be written here
         // ↓↓↓↓↓↓↓↓
+        petDatabaseHelper = new DatabaseHelper(rootView.getContext());
 
         //dataTesting button function
         Button btn_dataTesting = (Button) rootView.findViewById(R.id.data_testing);
@@ -52,7 +55,7 @@ public class MiniFragment extends Fragment {
         btn_shoppage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getActivity(), ShopPage.class));
+                createShopDialog(rootView.getContext());
             }
         });
 
@@ -115,5 +118,91 @@ public class MiniFragment extends Fragment {
             }
         });
 
+    }
+
+    private void createShopDialog(Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        final View shop_pop_up_view = getLayoutInflater().inflate(R.layout.shop_pup_up, null);
+
+        Button btn_shop_pop_up_cancel = shop_pop_up_view.findViewById(R.id.btn_close_shop);
+        Button btn_buy_food = shop_pop_up_view.findViewById(R.id.btn_buy_food);
+        Button btn_buy_toy = shop_pop_up_view.findViewById(R.id.btn_buy_toy);
+
+        TextView text_currency = shop_pop_up_view.findViewById(R.id.textView_currency);
+        TextView text_toy_price = shop_pop_up_view.findViewById(R.id.textView_foodPrice);
+        TextView text_food_price = shop_pop_up_view.findViewById(R.id.textView_toyPrice);
+        TextView text_foodNum = shop_pop_up_view.findViewById(R.id.textView_food_num);
+        TextView text_toyNum = shop_pop_up_view.findViewById(R.id.textView_toy_num);
+
+
+        int totalCurrency = petDatabaseHelper.getCurrency();
+        int numOfFood = petDatabaseHelper.getFood();
+        int numOfToy  = petDatabaseHelper.getToy();
+        int foodPrice = 10;
+        int toyPrice = 10;
+        text_currency.setText(totalCurrency + "");
+        text_food_price.setText(foodPrice + "");
+        text_toy_price.setText(toyPrice + "");
+        text_foodNum.setText(numOfFood + "");
+        text_toyNum.setText(numOfToy + "");
+
+        btn_buy_food.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int newCurrency = buyFood(foodPrice, shop_pop_up_view);
+                text_currency.setText(newCurrency + "");
+                int newNumFood = petDatabaseHelper.getFood();
+                text_foodNum.setText(newNumFood + "");
+            }
+        });
+        btn_buy_toy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int newCurrency = buyToy(toyPrice, shop_pop_up_view);
+                text_currency.setText(newCurrency + "");
+                int newNumToy = petDatabaseHelper.getToy();
+                text_toyNum.setText(newNumToy + "");
+            }
+        });
+        builder.setView(shop_pop_up_view);
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+        btn_shop_pop_up_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+    }
+
+    //got the food price and view of shop, return the rest of currency
+    private int buyFood(int foodPrice, View view) {
+        int totalCurrency = petDatabaseHelper.getCurrency();
+        int numOfFood = petDatabaseHelper.getFood();
+        if(totalCurrency >= foodPrice){
+            totalCurrency = totalCurrency - foodPrice;
+            numOfFood ++;
+            petDatabaseHelper.updateCurrency(totalCurrency);
+            petDatabaseHelper.updateFood(numOfFood);
+        }else{
+            Toast.makeText(view.getContext(), "You do not have enough currency", Toast.LENGTH_SHORT).show();
+        }
+        return totalCurrency;
+    }
+
+    //got the food price and view of shop, return the rest of currency
+    private int buyToy(int toyPrice, View view) {
+        int totalCurrency = petDatabaseHelper.getCurrency();
+        int numOfToy = petDatabaseHelper.getToy();
+        if(totalCurrency >= toyPrice){
+            totalCurrency = totalCurrency - toyPrice;
+            numOfToy ++;
+            petDatabaseHelper.updateCurrency(totalCurrency);
+            petDatabaseHelper.updateToy(numOfToy);
+        }else{
+            Toast.makeText(view.getContext(), "You do not have enough currency", Toast.LENGTH_SHORT).show();
+        }
+        return totalCurrency;
     }
 }
