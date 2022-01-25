c78e375ebb044eb407a9da51700e7347438ba795
DESKTOP-S2SKHJS\forsk <forskyrimdemacia@gmail.com>
2022-01-24 23:21:37 -0500

    fix lagging issue in trophy, replace image loader with Glide

--- app/src/main/java/com/example/duelt/popWindows/PopWindow.java
+++ app/src/main/java/com/example/duelt/popWindows/PopWindow.java
@@ -1,8 +1,10 @@
 package com.example.duelt.popWindows;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
@@ -10,8 +12,16 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.load.resource.gif.GifDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.example.duelt.Calculation;
 import com.example.duelt.LoadingActivity;
 import com.example.duelt.R;
@@ -156,9 +166,23 @@ public class PopWindow extends AppCompatActivity {
 
     //Use for avoiding repetitive code
     private void cupAnimation(){
-        vWinnerCup.setBackgroundResource(R.drawable.winner_cup_list);
-        mWinnerCup = (AnimationDrawable) vWinnerCup.getBackground();
-        mWinnerCup.start();
+        Glide.with(this)
+                .asGif()
+                .load(R.drawable.trophy)
+                .listener(new RequestListener<GifDrawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<GifDrawable> target, boolean isFirstResource) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(GifDrawable resource, Object model, Target<GifDrawable> target, DataSource dataSource, boolean isFirstResource) {
+                        resource.setLoopCount(1);
+                        return false;
+                    }
+                })
+                .fitCenter()
+                .into(vWinnerCup);
     }
 
     private void dueDateAnimation(int penalty){
